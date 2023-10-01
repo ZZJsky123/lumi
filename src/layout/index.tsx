eb12fbbd05e70df85667997cebfe0ba0fbfabfd4
ZZJsky123 <1647372420@qq.com>
2023-10-01 09:35:29 +0800

    feat:  css & static source

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,11 +1,19 @@
 import { useState } from "react";
 
+import viteSvgSrc from "../../public/vite.svg";
+import "./global.css";
+import styles from "./index.module.css";
+
 export function Layout() {
   const [count, setCount] = useState(0);
+
+  console.log("ViteSvg", viteSvgSrc);
+
   return (
-    <div>
+    <div className="layout-background">
       <h1>This is Layout Component</h1>
-      <div>
+      <img src={viteSvgSrc}></img>
+      <div className={styles["layout-header"]}>
         {count}
         <button onClick={() => setCount(count + 1)}>Add Count</button>
       </div>
